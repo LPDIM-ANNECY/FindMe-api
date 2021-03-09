011ca3a62b49d7c5fd6cf7e0bd98022e36c8a2d4
Nathan Cuvellier <cuvellier.nathan@gmail.com>
2021-03-09 10:09:01 +0100

    add config for build jar

--- src/Application.kt
+++ src/Application.kt
@@ -21,7 +21,7 @@ import org.jetbrains.exposed.sql.transactions.transaction
 @kotlin.jvm.JvmOverloads
 fun Application.myapp(testing: Boolean = false) {
 
-    Database.connect("jdbc:postgresql://127.0.0.1:5432/postgres", driver = "org.postgresql.Driver", user ="postgres", password = "442442")
+    Database.connect("jdbc:postgresql://127.0.0.1:5432/postgres", driver = "org.postgresql.Driver", user ="postgres", password = "test200")
     val user_findme_repository = UserFindmeRepository()
 
     transaction {
@@ -29,7 +29,8 @@ fun Application.myapp(testing: Boolean = false) {
         SchemaUtils.create(Place)
         SchemaUtils.create(User_itinerary)
 
-        /*User_findme.insert {
+        /*
+        User_findme.insert {
             it[User_findme.email] = "test@gmail.com"
             it[User_findme.password] = "test"
             it[User_findme.locale] = "France"
@@ -38,8 +39,7 @@ fun Application.myapp(testing: Boolean = false) {
             it[User_findme.total_distance] = 26.6f
             it[User_findme.picture] = "test picture"
             it[User_findme.pseudo] = "bat"
-        }*/
-/*
+        }
         Place.insert {
             it[Place.name] = "Tour eiffel2"
             it[Place.latitude] = 48.455f
@@ -47,9 +47,9 @@ fun Application.myapp(testing: Boolean = false) {
             it[Place.difficulty] = 2
             it[Place.radius_type] = "50"
             it[Place.active] = true
-        }*/
+        }
 
-        /*User_itinerary.insert {
+        User_itinerary.insert {
             it[User_itinerary.user_id] = 1
             it[User_itinerary.place_id] = 1
         }*/
