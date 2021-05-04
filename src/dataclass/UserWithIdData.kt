45f183bc14185747d4b4ca6c24d7d300ae2e75f5
BadTripHD <baptistepollet7@gmail.com>
2021-05-04 15:34:29 +0200

    change return data in route users/{id}

---
+++ src/dataclass/UserWithIdData.kt
@@ -0,0 +1,7 @@
+package fr.find.dataclass
+
+import java.time.LocalDate
+
+data class UserWithIdData(val id: Int,
+                          val picture: String,
+                          val pseudo: String)
--- src/repository/UserFindmeRepository.kt
+++ src/repository/UserFindmeRepository.kt
@@ -1,6 +1,7 @@
 package fr.find.repository
 
 import fr.find.dataclass.UserData
+import fr.find.dataclass.UserWithIdData
 import fr.find.entity.User_findme
 import io.ktor.http.*
 import org.jetbrains.exposed.sql.select
@@ -34,23 +35,13 @@ class UserFindmeRepository {
         return users
     }
 
-
-    //TODO refactor single user
-    fun getUserById(id: Int): ArrayList<UserData>{
-        val user : ArrayList<UserData> = arrayListOf()
+    fun getUserById(id: Int): ArrayList<UserWithIdData>{
+        val user : ArrayList<UserWithIdData> = arrayListOf()
         transaction {
             User_findme.select { User_findme.id eq id }.map {
                 user.add(
-                    UserData(
+                    UserWithIdData(
                         id = it[User_findme.id],
-                        email = it[User_findme.email],
-                        password = it[User_findme.password],
-                        created_at = it[User_findme.created_at],
-                        updated_at = it[User_findme.updated_at],
-                        locale = it[User_findme.locale],
-                        enable = it[User_findme.enable],
-                        level = it[User_findme.level],
-                        total_distance = it[User_findme.total_distance],
                         picture = it[User_findme.picture],
                         pseudo = it[User_findme.pseudo]
                     )
